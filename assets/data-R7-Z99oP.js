37579d97bf5eb3db05c112ddbc3e6e81f7fa7858
mohammadvaladbiegi2 <mohammadvaladbiegi2@users.noreply.github.com>
2024-01-09 11:05:43 +0000

    deploy: da5985dc9fb2a2648477a32a446b4e2764498df9

---
+++ assets/data-R7-Z99oP.js
@@ -0,0 +1,96 @@
+const e=[{id:1,title:"55متریدوطبقه نایسر",price:72e7,description:"55متری طبقه اول تکمیل دوم آماده گچ کاری خ دکل سوم",CREATED_AT:"1402,08,29",phone:"09180000000",category:"home",city:"sanandaj",img:"/Divarwebpage/public/images/e8ec7d4c-5988-487c-9e45-4cd98b90d47f.jpg",shortname:"homesanandaj1",function:null,condition:"تازه ساخت",meterage:"55"},{id:2,title:"تریل فلات ۲۰۰ نیوفیس",price:12e7,description:`سلام\r
+خشک خشک مدارک تکمیل بیمه ،کارت سوخت کارت موتور برگ سبز آماده انتقال`,CREATED_AT:"1402,08,30",phone:"09124444444",category:"vehicle",city:"sanandaj",img:"/Divarwebpage/public/images/362d9ea1-ef00-487a-a0e7-0fe31fbd6698.jpg",shortname:"vehicle1",function:"1000",condition:"سالم",meterage:null},{id:3,title:"اپل iPhone 7 با حافظهٔ ۱۲۸ گیگابایت",price:18e5,description:`آیفون ۷ باتری ۱۰۰\r
+بیس باند پرونده و نیازمند تعمیره\r
+مناسب تعمیرکارها\r
+کارتن موجود`,CREATED_AT:"1402,08,30",phone:"09120000000",category:"Digital",city:"sanandaj",img:"/Divarwebpage/public/images/4fe51bad-ddbe-4443-84bf-cab12d0ec175.jpg",shortname:"phone1",function:"دسته دوم",condition:"نیاز به تعمیر",meterage:null},{id:4,title:"مبل ال با میزعسلی",price:65e5,description:"مبل ال با میزعسلی",CREATED_AT:"1402.08.30",phone:"09110000000",category:"kitchen",city:"tehran",img:"/Divarwebpage/public/images/458824d6-c91d-42c3-830f-3915364028a1.jpg",shortname:"Sofa1",function:"دسته دوم",condition:"سالم",meterage:null},{id:5,title:"فروش شلغم و ترب اوشینی ارگانیک (ژاورود)",price:25e3,description:`یه خانم مهندس کشاورزی هستم ترب اوشینی و شلغم تازه برداشت شده، بدون استفاده از کود و سم، آبیاری با آب چاه، براتون آماده کردم
+
+توجه:
+قیمت کالا مقطوع است و تخفیف ندارد.`,CREATED_AT:"1402.09.01",phone:"01234567891",category:"kitchen",city:"tehran",img:"/Divarwebpage/public/images/b9ceb89c-0d52-42a8-bf31-c6a8a6f661d1.jpg",shortname:"turnip",function:null,condition:"سالم بهداشتی",meterage:null},{id:6,title:"خدمات تعمیرات پکیج لباسشویی یخچال ابگرمکن تعمیرکار",price:0,description:"باسلام تعمیر انواع مدل های پکیج ولباسشویی ایرانی خارجی تعمیرات انواع یخچال درمحل درکمترین زمان هزینه تمامی نقاط شهر سنندج",CREATED_AT:"1402.09.01",phone:"01234567898",category:"services",city:"tehran",img:"/Divarwebpage/public/images/d9wS0oVLMnP9oxCKJUtCeA.jpg",shortname:"laundryservice",function:"10 سال سابقه",condition:null,meterage:null},{id:7,title:"هودی اسپرت",price:28e4,description:`هودی اسپرت پسرانه ودخترانه جنس دورس گرم مدل ازاد\r
+سایز۶۰تا۶۵برای سن ۱۰تا۱۱سال اندازه میشه\r
+معاوضه ندارم\r
+در حدنو یک بار پوشیده شده به دلیل سایز نبودن فروخته میشه\r
+\r
+توجه:\r
+قیمت کالا مقطوع است و تخفیف ندارد.`,CREATED_AT:"1402.09.02",phone:"01234567897",category:"personalitems",city:"tehran",img:"/Divarwebpage/public/images/5b4275ea-83e5-4e06-b2d9-c983e40b16f2.jpg",shortname:"hoodie",function:"اکبند",condition:"سالم اکبند",meterage:null},{id:8,title:"سهره قناری دهن ایوب رحمتی جدید",price:35e5,description:"با سلام خدمت تمامی عشق بازان عزیز جوجه سهره قناری ۵ماهه تازه تخت دهن ایوب رحمتی جدید و دکتر بیات بیا مهر تقریبلا همه دهن هارو گرفته بدون غلط تازه داره بر پا میشه کوتاه بلند میخونه ولی دهن هاش واضحه فقط یک بیا مادریم داره لان خودم بوده شاه جوجه طوسی رنگ سالم و سرحال باهاش کار کنی مست میشه من بدلیل مشغله کاری بهش نمی رسم پول یه سهره باغی عادی گزاشتم مست بشه قیمتش خیلی از اینا زیاد تره پس دادشم الکی مزاحم نشو (فروش بدون قفس ارسالم دارم )با تشکر. یه فیلم کوتاه دارم روبیکا پیام بدین چشم.",CREATED_AT:"1402.09.02",phone:"01234567895",category:"entertainment",city:"Kermanshah",img:"/Divarwebpage/public/images/427b280f-4c9b-4bda-aabc-db4cbe4bda2e.jpg",shortname:"bird",function:null,condition:"سالم",meterage:null},{id:9,title:"پیدا شده",price:0,description:"امروز پیداش کردم صاحبش بیاد تحویل بگیره",CREATED_AT:"1402.09.03",phone:"01234567894",category:"social",city:"Kermanshah",img:"/Divarwebpage/public/images/aeddb812-4563-4c1c-9d29-79107a32f3db.jpg",shortname:"dog",function:null,condition:"گم شده",meterage:null},{id:10,title:"یخچال ایستاده",price:27e6,description:"یخچال ایستاده چهار در یکطرف شیشه کاملا سالم تمیز ضمانتی",CREATED_AT:"1402.09.03",phone:"01234567892",category:"industrialequipment",city:"Kermanshah",img:"/Divarwebpage/public/images/86d07656-2dc9-4932-87a6-8d99e16a6b2b.jpg",shortname:"Refrigerator",function:"اکبند",condition:"سالم",meterage:null},{id:14,title:"طبقه اول 5 اذر",price:2e4,description:"خانه عالی",CREATED_AT:"۱۴۰۲/۹/۱۱",phone:"09106578534",category:"home",city:"sanandaj",img:"/Divarwebpage/public/images/81cf4e1a-8372-455c-a693-77905381a281.jpg",shortname:"e954c0",function:"",condition:"تازه ساخت",meterage:"150"},{id:15,title:"لپ تاپ",price:3e7,description:`لپ تاپ سالم بدون ایراد 
+RAM 6
+core i7
+1TB SSD`,CREATED_AT:"۱۴۰۲/۹/۱۱",phone:"01234567899",category:"Digital",city:"tehran",img:"/Divarwebpage/public/images/c97d83b1-b0ba-4716-8412-b9b4d8303fea.jpg",shortname:"c81d04",function:"تازه",condition:"سالم",meterage:""},{id:16,title:"ماشین شارژی فورد لانگ دو موتور بزرگ",price:127e5,description:`توضیحات
+سنندج، بلوار خضر زنده، حد فاصل میدان ظفریه به سمت‌میدان شورا، فروشگاه دوچرخه و ماشین شارژی پیام
+______________
+✅قیمت ها رو حضوری مقایسه کنید…
+______________
+ماشین شارژی دو موتوره
+صندلی چرم
+کمک فنر دار
+پخش فلش خور
+کنترل دار برای والدین
+مناسب تا کودک 8 ساله
+چراغ جلو و عقب
+پروژکتور دار
+تمام فول
+سه سرعته
+__________
+تمام سایز ها موجود است`,CREATED_AT:"۱۴۰۲/۹/۱۱",phone:"12345678903",category:"personalitems",city:"Kermanshah",img:"/Divarwebpage/public/images/79f9bafa-0e39-4491-8477-5b9100114942.jpg",shortname:"fe6a74",function:"تازه",condition:"سالم",meterage:""},{id:17,title:"عروس هلندی گمشده",price:0,description:"عروس هلندی در محدوده تالار رز نزدیک تعویض روغنی شبانه روزی آرمین ناصری گمشده از یابنده خواهشمندم در صورت پیدا کردن تماس بگیرد و مژدگانی دریافت کند",CREATED_AT:"۱۴۰۲/۹/۱۱",phone:"01234567899",category:"social",city:"sanandaj",img:"/Divarwebpage/public/images/37236d43-a6f6-4a07-89eb-04dee62197e3.jpg",shortname:"e7de75",function:"",condition:"",meterage:""},{id:18,title:"موتور برق ایزی پاور چین",price:22e6,description:`♧ توان ۸۵۰۰ واقعی
+♧ قدرت موتور ۱۸ اسب بخار
+♧ حجم موتور ۴۶۰ سی سی
+♧ سیم پیچی تمام مس
+♧ دارای تثبیت ولتاژ A.V.R یا نوسان گیر
+♧ استارتی به همراه چرخ و دسته و باطری
+♧ دارای سیستم هوشپند کمبود روغن
+♧ ظرفیت مخزن سوخت ۲۵ لیتر بنزینی
+♧ موتور از نوع هواخنک ، چهار زمانه و تکفاز
+◇ ولتلژ خروجی ۲۲۰ ولت ، ولتاژ خروجی ۱۲ ولت ، وشدت جریان خروچی ۸.۳ آمپر برای خروجی DC و حدود ۳۲ آمپر برای خروجی AC`,CREATED_AT:"۱۴۰۲/۹/۱۱",phone:"01234567899",category:"industrialequipment",city:"sanandaj",img:"/Divarwebpage/public/images/fd335c37-13e6-4642-8035-5703e250e979.jpg",shortname:"d32ff7",function:"تازه",condition:"سالم",meterage:""},{id:21,title:"عروسک جوجه نانو",price:23e4,description:`عروسک جوجه با پارچه نانو خارجی
+
+تولیدشده با دستگاههای اتوماتیک
+
+قابل شستشو ،ضد حساسیت ،بدون پرزدهی ،
+
+پرشده با الیاف لایکو
+
+درصورت عدم رضایت تضمین برگشت محصول داریم بدون قید و شرط
+
+تولید انواع عروسک پولیشی
+
+ارسال رایگان درب منزل سراسر کشور`,CREATED_AT:"۱۴۰۲/۹/۱۱",phone:"01234567899",category:"entertainment",city:"tehran",img:"/Divarwebpage/public/images/c0143d02-f199-4c06-868d-3c93d72f2c61.jpg",shortname:"e59558",function:"تازه",condition:"سالم",meterage:""},{id:24,title:"اکسسوری مردانه جذاب",price:1e5,description:`قیمت عکس اول ۱۸۹ تومن جنس استیل ضد زنگ
+قیمت عکس ۲ و ۳ و ۴ و پنج ۱۹۹ تومن جنس استیل ضد زنگ
+قیمت عکس ششم ۱۷۹ تومن جنس استیل ضد زنگ
+قیمت عکس هفتم ۱۵۹ تومن جنس پلیمری
+قیمت عکس هشتم ۱۶۹ تومن جنس استیل ضد زنگ
+قیمت عکس نهم ۱۹۹ تومن جنس استیل ضد زنگ
+
+پرداخت درب منزل
+برای خرید عکس محصول و شماره تماس بفرستید.
+
+توجه:
+قیمت کالا مقطوع است و تخفیف ندارد.`,CREATED_AT:"۱۴۰۲/۹/۱۱",phone:"01234567899",category:"personalitems",city:"Kermanshah",img:"/Divarwebpage/public/images/cef2267b-cfc8-46e1-bdbd-1229c41c634d.jpg",shortname:"d38904",function:"تازه",condition:"سالم",meterage:""},{id:25,title:"ام‌وی‌ام 530، مدل ۱۳۹۰",price:27e7,description:`جلوبندی،تسمه تایم و کمک ها به تازگی تعویض شده
+شاسیها،سقف و ستون پلمپ
+سانروف سالم
+موتور ۲۰۰۰
+۱۶ سوپاپ
+پر قدرت و پر شتاب
+فنی سالم
+درب عقب سمت شاگرد مالیدگی داره`,CREATED_AT:"۱۴۰۲/۹/۱۲",phone:"01234567899",category:"vehicle",city:"Kermanshah",img:"/Divarwebpage/public/images/c324d3cc-9d30-40b4-9663-7263bf7c64f1.jpg",shortname:"d98d70",function:"230000",condition:"سالم",meterage:""},{id:27,title:"پژو پارس ELX، مدل ۱۳۸۵ موتور زانتیا",price:41e7,description:`با سلام
+پژو پارس یی ال ایکس مدل ۱۳۸۵
+دو تکه و نیم رنگ
+کف صندوق از چپ مقداری قلم گیری دارد
+سینه کرم
+صندلی خلبانی تمام فول صفر ۸ حالته
+فنی موتوری سالم بدون مشکل
+موتور و اتاق فابریک به شرط کارشناسی
+چهار حلقه لاستیک نو
+بیمه تا فروردین ۱۴۰۳
+چهار حلقه رینگ خرچنگی اصل ایتالیا
+بدون آفتاب سوختگی یا کیلر پران
+شیشه جلو نو دو سه روزه تعویض شده
+بدون یک ریالی خرج درجه یک در حد
+زاپاس خرچنگی
+شتاب عالی
+بدون بخار و آب کم کردن
+بدون یک قطره روغن سوزی یا روغن ریزی
+ماشین خیلی تمیز و سالمه
+کاملا خانگی دو سه هفته یه بار از پارکینگ بیرون میاد
+چراغ جلو و عقب نو
+مشتری واقعی تماس بگیره
+کلا باید elx باز باشی تا بدونی یی ال ایکس سالم و بدون ایراد و مشکل و بدون خرج قیمتش بالاس
+توضیحات دیگری اگه لازمه تماس بگیرین عرض میکنم خدمتتون
+بازدید سنندج`,CREATED_AT:"۱۴۰۲/۹/۱۲",phone:"01234567899",category:"vehicle",city:"sanandaj",img:"/Divarwebpage/public/images/bdbe6a75-611e-4ac4-84f5-59863b35dc00.jpg",shortname:"a88dd9",function:"386000",condition:"سالم",meterage:""},{id:28,title:"کوییک ساده۹۹",price:2e7,description:"ماشین فوق در شهرستان سقز می باشد جهت بازدید بدون‌رنگ شاسی عقب و جلو بدون ترک و جوش وپلمپ متورهم به ضمانت مکانیک فروش فقط نقدی و معاوضه‌ندارم",CREATED_AT:"۱۴۰۲/۹/۱۵",phone:"01234567899",category:"vehicle",city:"sanandaj",img:"/Divarwebpage/public/images/41d9d78b-386c-4c5e-9e34-13f5160b7a07.jpg",shortname:"1bea7e",function:"230000",condition:"سالم",meterage:""},{id:29,title:"موتور احسان شکاری مدل 88",price:24e6,description:"باسلام موتور احسان شکاری 150مدارک تکمیل فاقد کارت سوخت",CREATED_AT:"۱۴۰۲/۹/۱۹",phone:"01234567899",category:"vehicle",city:"sanandaj",img:"/Divarwebpage/public/images/be1e9cf9-fd75-4674-bde8-18ed6ec7faff.jpg",shortname:"b63c25",function:"اوراقی",condition:"نیاز به تعمیر",meterage:""}];export{e as d};
